365ca846f9438455de63f8582cc7e180a5e607ac
gaudelbijay <gaudelbijay10@gmail.com>
2020-08-18 14:26:26 +0545

    file directory

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -8,8 +8,6 @@ func crawl_over_page:
 
 Input: url to main pages, html_post_tag in main pages
 
-main body: find all the links to the related html_post_tag
-
 output: return list of all url for html_post_tag
 '''
 
@@ -27,28 +25,29 @@ func scrapper:
 Input: url from crawl_over_pages function, html_post_tag in post pages, starting_tag_number, 
 ending_tag_number
 
-output: .txt file containgng all the post 
+output: write on .txt file containgng all the post 
 '''
 
-def scrapper(url, starting_p=3, ending_p=-10, section='p'):
+def scrapper(url,starting_p = 3, ending_p = -10, section='p', my_dir = '../data', file_to_save='stories.txt'):
     urls = crawl_over_page(url)
     post = ''
     for url in urls:
         response = requests.get(url)
         content = BeautifulSoup(response.content, "html.parser")
         content = content.findAll(section)
-
+        
         for each_p in content[starting_p:ending_p]:
-            post = post + each_p.text
-
-        with open('stories.txt', encoding='utf-8', mode='w') as f:
+            post = post+each_p.text
+        
+        if not os.path.exists(my_dir):
+            os.makedirs(my_dir)
+            
+        with open(my_dir+'/'+file_to_save, encoding='utf-8', mode='w') as f:
             f.write(post)
-
 
 # run the code
 if __name__ == '__main__':
     url = 'http://inepal.org/nepalistories/tag/read-nepali-stories-online/?fbclid=IwAR15RcMQqjA6LdJ5wyGKwiC_VdmYu-4FP1X1wBoeKfw0uOvuYEkgg-xJhgo'
-
     scrapper(url)
 
 
