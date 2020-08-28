dbbd03c25e6029690254a9c7e4972a050f988415
gaudelbijay <gaudelbijay10@gmail.com>
2020-08-28 15:27:58 +0545

    remove optimizer from this file

--- wordgenerator/main.py
+++ wordgenerator/main.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
     vocab_len = len(vocab)
     idx2vocab = np.array(vocab)
     X, Y = utils.tensor_from_sentences(sentence, vocab)
-    batches_X, batches_Y = utils.batchify(X, Y)
+    # batches_X, batches_Y = utils.batchify(X, Y)
     embed_size = 50
 
     network = RNN(input_size=vocab_len, embed_dim=embed_size, hidden_size=1000, output_size=vocab_len,
                   n_layers=2)
-    optimizer = torch.optim.Adam(network.parameters())
+    # optimizer = torch.optim.Adam(network.parameters())
     loss_fuction = nn.KLDivLoss()
-    train(network, optimizer, loss_fuction, 1, batches_X, batches_Y)
+    train(network, loss_fuction, 1, X, Y)
     t = 250
     seq = utils.tensor_from_single_batch(sentence[t], vocab)
     print(sentence[t])
